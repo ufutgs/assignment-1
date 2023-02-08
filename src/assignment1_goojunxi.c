1bb22823e40fad52cf5d29246cb168ab02f10a13
ufutgs <36780390+ufutgs@users.noreply.github.com>
2023-02-08 13:27:00 +0800

    a

--- src/assignment1_goojunxi.c
+++ src/assignment1_goojunxi.c
@@ -10,18 +10,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>
 #include "assignment1_goojunxi.h"
 
-player** player_list;
-monster** monster_list;
 
 int main(void) {
+	struct pList plist;
+	struct mList mlist;
+	int i;
 	char c = 'c';
-	player * p1 = newPlayer("come");
+	list_init(&plist,&mlist);
 	update_boolean_value(&c, 0, 0);
 	printf("updated c is %c\n", c);
 	print_boolean(&c, 1);
-	free(p1);
+	list_free(plist, mlist);
 	return 0;
 }
 
@@ -48,11 +51,6 @@ void print_boolean(char *booleans, int index)
 	printf("value: %d\n",(i>1 || i<0) ? 1 : i);
 }
 
-char * print_class(player *p)
-{
-	return &p->status;
-}
-
 void printPlayerInformation (player *p)
 {
 	char *ptr = &(p->status);
@@ -68,14 +66,128 @@ void printPlayerInformation (player *p)
 
 }
 
+char * print_class(player* p)
+{
+	int class = 0;
+	int i;
+	for(i=0;i<3;i++)
+	{
+		class += get_boolean_value(p->status, i)*pow(2,i);
+	}
+	switch(i){
+	case 0:
+		return "Warrior";
+	case 1:
+		return "Mage";
+	case 2:
+		return "Rogue";
+	case 3:
+		return "Priest";
+	case 4:
+		return "Monk";
+	case 5:
+		return "Druid";
+	case 6:
+		return "Beserker";
+	case 7:
+		return "Paladin";
+	default:
+		return "Invalid class";
+	}
+
+}
+
+void attackMonster (player *p, monster *m)
+{
+
+}
+
 player * newPlayer(char *name)
 {
-	player *p = malloc(sizeof(player));
+	char stat = (char) 0;
+	int i;
+	int atk_type;
+	int random;
+	atk_type=0;
+	player *p = (player*)malloc(sizeof(player));
 	p->name = name;
-	p->hp = rand()%200 + 100;
-	p->atk = rand()%20 + 5;
-	p->def = rand()%30;
+	p->hp = rand()%201 + 100;
+	p->atk = rand()%21 + 5;
+	p->def = rand()%31;
 	p->status_track = (char) 0;
-
+	for(i=0;i<3;i++)
+	{
+		random = rand()%2;
+		update_boolean_value(&stat, i, random);
+		atk_type += random*pow(2,i);
+	}
+	update_boolean_value(&stat,3,atk_type%2);
+	p->status = stat;
+	p->printPlayerInformation = &printPlayerInformation;
+	p->attackMonster = &attackMonster;
+	p->print_class = &print_class;
+	p->newPlayer = &newPlayer;
 	return p;
 }
+
+void printMonsterInformation (monster *m)
+{
+
+}
+
+monster * newMonster()
+{
+	monster* m = (monster*)malloc(sizeof(monster));
+	m->hp = rand()%1001 + 1000;
+	m->atk = rand()%21+5;
+	m->def = rand()%11;
+	m->id = (int) time(NULL);
+	m->status_rate = 4;
+	m->printMonsterInformation = &printMonsterInformation;
+	m->newMonster = &newMonster;
+	m->attackPlayer = & attackPlayer;
+	return m;
+}
+
+int attackPlayer(player *p, monster *m)
+{
+
+}
+
+void list_free(struct pList* plist,struct mList* mlist)
+{
+	int i;
+	for(i=0;i<plist->size;i++)
+	{
+		free(plist->list[i]);
+	}
+	free(plist->list);
+	for(i=0;i<mlist->size;i++)
+	{
+		free(mlist->list[i]);
+	}
+	free(mlist->list);
+}
+
+void list_init (struct pList* plist,struct mList* mlist)
+{
+	int i;
+	struct player p;
+	struct monster m;
+	p.newPlayer = &newPlayer;
+	plist->list = (player**)malloc(4*sizeof(player*));
+	mlist->list = (monster**)malloc(6*sizeof(monster*));
+	plist->size=0;
+	mlist->size=0;
+	for(i =0; i<4; i++)
+	{
+		plist->list[i] = p.newPlayer("test");
+		plist->size+=1;
+	}
+	for(i =0; i<(rand()%6+1); i++)
+	{
+		mlist->list[i] = (monster*)malloc(sizeof(monster));
+		mlist->size+=1;
+	}
+}
+
--- src/assignment1_goojunxi.h
+++ src/assignment1_goojunxi.h
@@ -13,6 +13,16 @@ void print_boolean(char *booleans, int index);
 
 typedef struct player player;
 typedef struct monster monster;
+typedef struct playerList pList;
+typedef struct monsterList mList;
+typedef struct charList cList;
+
+/*
+status boolean = [class][attack type][status]
+status_check = [Poison][Stun][Silent][Blind]
+attack type : 1 = magical attack, 0 = physical attack
+*/
+
 struct player
 {
 	char * name;
@@ -30,11 +40,31 @@ struct player
 {
 	int hp,atk,def,id;
 	int status_rate;
-	void (*printPlayerInformation) (monster *m);
+	void (*printMonsterInformation) (monster *m);
 	monster * (*newMonster)();
 	int (*attackPlayer)(player *p, monster *m);
 };
 
+struct pList
+{
+	player** list;
+	int size;
+	void (*init) (int i);
+};
+
+struct mList
+{
+	monster** list;
+	int size;
+	void (*init) (int i);
+};
+
+struct cList
+{
+	char** list;
+	int size;
+	void (*init) (int i);
+};
 
 void printPlayerInformation (player *p);
 void attackMonster (player *p, monster *m);
@@ -47,4 +77,7 @@ void playerAttackLog(monster* m);
 void printMonsterInformation (monster *m);
 monster * newMonster();
 int attackPlayer(player *p, monster *m);
+
+void list_free(struct pList* plist,struct mList* mlist);
+void list_init (struct pList* plist,struct mList* mlist);
 #endif /* ASSIGNMENT1_GOOJUNXI_H_ */
